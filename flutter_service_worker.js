0412d2455909750d388dc6c4d60485f4b87ebc0c
Ivan MacOS <fortran.new@gmail.com>
2026-03-09 10:00:42 +0700

    Ошибка в мобильной версии

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5cdde87b97d396dd158516bc7aec7ca9",
+const RESOURCES = {"flutter_bootstrap.js": "7f0ed6969da30580ca335d16d735eb72",
 "version.json": "db9bca8928fb94a1eb13ba67c1cdf4d8",
 "index.html": "aace361e7f113539f8814ecacfe0956b",
 "/": "aace361e7f113539f8814ecacfe0956b",
-"main.dart.js": "21eabe2c204c522fb47ae0a385316b4a",
+"main.dart.js": "963d3d3fb9639f7a7f161ff191978d9b",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "96c21dbc43b79812399dc8497b66ac02",
 "icons/Icon-192.png": "e27cb8cf97cb5482cd48abea720ed31d",
