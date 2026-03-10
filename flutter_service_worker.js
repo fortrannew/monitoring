bce8c9fac3897d59fcecc6679e201dc4e50eaf00
Ivan MacOS <fortran.new@gmail.com>
2026-03-10 08:58:45 +0700

    Для помещений возможность добавлять устройства

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7f0ed6969da30580ca335d16d735eb72",
+const RESOURCES = {"flutter_bootstrap.js": "5b2bd422bd6e768e917eee8ac4071919",
 "version.json": "db9bca8928fb94a1eb13ba67c1cdf4d8",
 "index.html": "aace361e7f113539f8814ecacfe0956b",
 "/": "aace361e7f113539f8814ecacfe0956b",
-"main.dart.js": "963d3d3fb9639f7a7f161ff191978d9b",
+"main.dart.js": "3e33ead16428cb8e35ce50565039cbac",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "96c21dbc43b79812399dc8497b66ac02",
 "icons/Icon-192.png": "e27cb8cf97cb5482cd48abea720ed31d",
