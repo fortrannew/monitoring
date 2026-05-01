db4a4fae0c558a17a556ca62572d38b3093af8f4
Ivan MacOS <fortran.new@gmail.com>
2026-05-02 02:03:02 +0700

    Оборудование - Вывести основные данные датчиков

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1f1fed566b6f9ad45266623668f7caba",
+const RESOURCES = {"flutter_bootstrap.js": "a33bc00dceeea002be0b8898158832da",
 "version.json": "db9bca8928fb94a1eb13ba67c1cdf4d8",
 "index.html": "aace361e7f113539f8814ecacfe0956b",
 "/": "aace361e7f113539f8814ecacfe0956b",
-"main.dart.js": "df6244945c8a25ddf1920a66f04e4112",
+"main.dart.js": "0e858b1493902feb3503f8a7b4ae5cc6",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "96c21dbc43b79812399dc8497b66ac02",
 "icons/Icon-192.png": "e27cb8cf97cb5482cd48abea720ed31d",
