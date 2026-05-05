482e10d02e9037a88a41bfb161771c129189b865
Ivan MacOS <fortran.new@gmail.com>
2026-05-06 04:55:23 +0700

    Графики

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b46565b96e1555718cf5e248d16552ab",
+const RESOURCES = {"flutter_bootstrap.js": "cdedde02689edc71ab56a20fb55486ac",
 "version.json": "db9bca8928fb94a1eb13ba67c1cdf4d8",
 "index.html": "aace361e7f113539f8814ecacfe0956b",
 "/": "aace361e7f113539f8814ecacfe0956b",
-"main.dart.js": "1eb882ad7cb844ff9c9ea0ca7810ed2d",
+"main.dart.js": "71ede489e49ff77631578d95ef836d7e",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "96c21dbc43b79812399dc8497b66ac02",
 "icons/Icon-192.png": "e27cb8cf97cb5482cd48abea720ed31d",
