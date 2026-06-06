3f3fda63350daf9a7e6e94a4f5d2a626d6c5ad1b
Ivan MacOS <fortran.new@gmail.com>
2026-06-07 03:26:12 +0700

    Привязка клиента к объекту, корпусу, плану здания, устройству

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c73096ddc134f71a84216cf1ed8b77af",
+const RESOURCES = {"flutter_bootstrap.js": "01558f74e4ab9d86ea1201d1ce2eaac1",
 "version.json": "db9bca8928fb94a1eb13ba67c1cdf4d8",
 "index.html": "aace361e7f113539f8814ecacfe0956b",
 "/": "aace361e7f113539f8814ecacfe0956b",
-"main.dart.js": "eeb5826a2ebe889211f7ed27d845a7f5",
+"main.dart.js": "cc538f4e49308054be348fabbd565cd9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "96c21dbc43b79812399dc8497b66ac02",
 "icons/Icon-192.png": "e27cb8cf97cb5482cd48abea720ed31d",
